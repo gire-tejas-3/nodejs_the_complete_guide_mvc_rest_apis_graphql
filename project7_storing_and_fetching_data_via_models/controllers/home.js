5977a286f0d9767586251f9212a5ec0f610e016c
giretejas <giretejas@gmail.com>
2024-02-02 19:34:35 +0530

    Storing Data in JSON And Fetching via  Models

--- project7_storing_and_fetching_data_via_models/controllers/home.js
+++ project7_storing_and_fetching_data_via_models/controllers/home.js
@@ -2,10 +2,28 @@ const data =  require('./product');
 const Product = require('../models/product');
 
 exports.getHome = (req, res, next) => {
-    const product = Product.fetchAll();
-    res.render("index.ejs", {
-        product,
-        title: "Home Page",
-        path: "/",
+    // const product = Product.fetchAll();
+    /*
+        res.render("product.ejs", {
+            product,
+            title: "Products",
+            path: "/product",
+        });
+    */
+    /* 
+        Product.fetchAll() was returning product from simple Array, but we are now storing data in json file.
+        so storing and reading data from file this procedure is happen Asynchronously (for no blocking of code mechanism)
+        it will firstly return undefined, so product is returnig as undefined to views, which occurs the error on browser 
+
+        so to minimise this error here pass callback function to retrive data from json file, if there is any error it 
+        will return an empty [] instead of undefined, and if data present in file it will call cb() with data as argument
+        and stored in product which will render to ejs page.
+    */
+    const product = Product.fetchAll(product => {
+        res.render("index.ejs", {
+            product,
+            title: "Home Page",
+            path: "/",
+        });
     });
 }; 
--- project7_storing_and_fetching_data_via_models/controllers/product.js
+++ project7_storing_and_fetching_data_via_models/controllers/product.js
@@ -1,7 +1,7 @@
-const Product =  require('../models/product');
+const Product = require("../models/product");
 
-exports.postProduct = (req, res, next) => { 
-    const product =  new Product(req.body.product);
+exports.postProduct = (req, res, next) => {
+    const product = new Product(req.body.product);
     product.save();
     res.redirect("/product");
 };
@@ -14,10 +14,29 @@ exports.getAddProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-    const product = Product.fetchAll();
-    res.render("product.ejs", {
-        product,
-        title: "Products",
-        path: "/product",
+    // const product = Product.fetchAll();
+    /*
+        res.render("product.ejs", {
+            product,
+            title: "Products",
+            path: "/product",
+        });
+    */
+
+    /* 
+        Above Product.fetchAll() was returning product from simple Array, but we are now storing data in json file.
+        so storing and reading data from file this procedure is happen Asynchronously (for no blocking of code mechanism)
+        it will firstly return undefined, so product is returnig as undefined to views, which occurs the error on browser 
+
+        so to minimise this error here pass callback function to retrive data from json file, if there is any error it 
+        will return an empty [] instead of undefined, and if data present in file it will call cb() with data as argument
+        and stored in product which will render to ejs page.
+    */
+    Product.fetchAll((product) => {
+        res.render("product.ejs", {
+            product,
+            title: "Products",
+            path: "/product",
+        });
     });
 };
