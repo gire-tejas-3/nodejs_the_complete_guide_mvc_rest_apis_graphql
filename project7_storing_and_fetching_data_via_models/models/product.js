7f5feedd77d5572a02fce70ee3bc83bdc4ce66bf
Tejas Gire <113277524+giretejas@users.noreply.github.com>
2024-02-02 19:35:13 +0530

    Storing Data in JSON And Fetching via  Models (#1)

--- project7_storing_and_fetching_data_via_models/models/product.js
+++ project7_storing_and_fetching_data_via_models/models/product.js
@@ -1,15 +1,37 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
+const dataPath = path.join(
+    path.dirname(require.main.filename), //process.mainModule is depricated , instead use require.main
+    "data",
+    "products.json"
+);
 module.exports = class Product {
     constructor(product) {
         this.product = product;
     }
 
     save() {
-        products.push(this);
-    };
+        let products = [];
+        fs.readFile(dataPath, (err, data) => {
+            if (!err) {
+                products = JSON.parse(data);
+            }
+            products.push(this);
+            // it will write data in json file
+            fs.writeFile(dataPath, JSON.stringify(products), (error) => {
+                console.log(error);
+            });
+        });
+    }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        // fetch all data from json file
+        fs.readFile(dataPath, (err, content) => {
+            if (err) {
+                return cb([]);
+            }
+            cb(JSON.parse(content));
+        });
     }
 };
