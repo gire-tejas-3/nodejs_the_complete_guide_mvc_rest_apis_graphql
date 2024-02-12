20eeac8ea204c798b64c627febe2a29d9b62e85a
giretejas <giretejas@gmail.com>
2024-02-12 12:20:03 +0530

    Dynamic Routing, Advance Model and Cookie Authentication

--- project9_dynamic_routing_advance_models/controller/index.js
+++ project9_dynamic_routing_advance_models/controller/index.js
@@ -2,7 +2,7 @@ const Register = require('../model/register');
 
 exports.getAdmin = (req, res, next) => {
 	const id = req.params.id;
-	const user = req.user ||  req.locals.user;
+	const user = req.user || req.locals.user;
 	Register.fetchAll((exsistingData) => {
 		res.render('admin', {
 			title: 'Admin',
@@ -15,22 +15,25 @@ exports.getAdmin = (req, res, next) => {
 
 exports.postUser = (req, res, next) => {
 	const id = parseInt(req.params.id);
-	const data =  req.body;
+	const data = req.body;
 	Register.update(id, data, (result) => {
 		res.redirect('/admin');
 	});
 };
 
 exports.deleteUser = (req, res, next) => {
-	const id =  parseInt(req.params.id);
-	if(id === req.user.id) return;
+	const id = parseInt(req.params.id);
+	if (id === req.user.id) {
+		res.redirect('/admin');
+		return;
+	}
 	Register.delete(id, (result) => {
 		console.log(result);
 		res.redirect('/admin');
-	})
-}
+	});
+};
 
 exports.getHome = (req, res, next) => {
-	const user = req.user ||  req.locals.user;
-	res.render('index', {title: 'Home', user});
+	const user = req.user || req.locals.user;
+	res.render('index', { title: 'Home', user });
 };
