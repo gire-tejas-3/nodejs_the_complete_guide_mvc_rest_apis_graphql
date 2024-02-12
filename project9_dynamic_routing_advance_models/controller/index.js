23c7ea50df430e8fffc44146111bfb98b93199ff
giretejas <giretejas@gmail.com>
2024-02-12 11:51:44 +0530

    Dynamic ROuting and Authentication with cookies

---
+++ project9_dynamic_routing_advance_models/controller/index.js
@@ -0,0 +1,36 @@
+const Register = require('../model/register');
+
+exports.getAdmin = (req, res, next) => {
+	const id = req.params.id;
+	const user = req.user ||  req.locals.user;
+	Register.fetchAll((exsistingData) => {
+		res.render('admin', {
+			title: 'Admin',
+			users: exsistingData,
+			user,
+			id,
+		});
+	});
+};
+
+exports.postUser = (req, res, next) => {
+	const id = parseInt(req.params.id);
+	const data =  req.body;
+	Register.update(id, data, (result) => {
+		res.redirect('/admin');
+	});
+};
+
+exports.deleteUser = (req, res, next) => {
+	const id =  parseInt(req.params.id);
+	if(id === req.user.id) return;
+	Register.delete(id, (result) => {
+		console.log(result);
+		res.redirect('/admin');
+	})
+}
+
+exports.getHome = (req, res, next) => {
+	const user = req.user ||  req.locals.user;
+	res.render('index', {title: 'Home', user});
+};
